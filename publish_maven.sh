6430406b75e9bf3aceb1aea5790e122fbb39e4d3
İlhan Atahan <ata.ilhan.official@gmail.com>
2026-08-31 19:23:58 +0300

    8036

--- publish_maven.sh
+++ publish_maven.sh
@@ -67,7 +67,7 @@ cat > "$DEST/$ARTIFACT-$VERSION.pom" <<EOF
   <licenses>
     <license>
       <name>BSD-3-Clause</name>
-      <url>https://chromium.googlesource.com/angle/angle/+/refs/heads/chromium/7871/LICENSE</url>
+      <url>https://chromium.googlesource.com/angle/angle/+/refs/heads/chromium/8036/LICENSE</url>
     </license>
   </licenses>
 </project>
